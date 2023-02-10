86b705515cf9f418a4d70e6904c4712b3801bdf3
Viktoriya Datchuk <grelada@tut.by>
2023-02-10 15:51:53 +0300

    feat: create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import type { ReactElement } from 'react'
+import './Header.css'
+import Accounts from '../../assets/img/accounts.png'
+import Contributions from '../../assets/img/contributions.png'
+import Expenses from '../../assets/img/expenses.png'
+import Income from '../../assets/img/income.png'
+import Loans from '../../assets/img/loans.png'
+import Planning from '../../assets/img/planning.png'
+import { HeaderButton } from '../HeaderButton/HeaderButton'
+import HeaderIcon from '../../assets/img/headerIcon.png'
+
+export const Header = (): ReactElement => {
+  const HeaderList = {
+    Счета: Accounts,
+    Расходы: Expenses,
+    Доходы: Income,
+    'Кредиты и Долги': Loans,
+    Планирование: Planning,
+    'Банковские вклады': Contributions,
+  }
+  return (
+    <header>
+      <div className="header__title">
+        <img src={HeaderIcon} alt={HeaderIcon} className="title__image" />
+        <h1>Домашняя бухгалтерия</h1>
+      </div>
+      <div className="header__container">
+        {Object.entries(HeaderList).map((button) => (
+          <HeaderButton image={button[1]} title={button[0]} key={button[0]} />
+        ))}
+      </div>
+    </header>
+  )
+}
--- src/components/HeaderButton/HeaderButton.tsx
+++ src/components/HeaderButton/HeaderButton.tsx
@@ -1,7 +1,8 @@
-import { HeaderButtonType } from '../../interfaces/propsTypes'
+import type { ReactElement } from 'react'
+import type { HeaderButtonType } from '../../interfaces/propsTypes'
 import './HeaderButton.css'
 
-export const HeaderButton = ({ image, title }: HeaderButtonType) => {
+export const HeaderButton = ({ image, title }: HeaderButtonType): ReactElement => {
   return (
     <button className="header__button">
       <img src={image} alt={image} className="button__image"></img>
